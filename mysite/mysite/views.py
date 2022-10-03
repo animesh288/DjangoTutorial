952e71c1e59ec9acfbbf5f226f51c0360386c932
animesh288 <animesh.satish@gmail.com>
2022-10-04 03:10:59 +0530

    bootstrap

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -18,7 +18,7 @@ def textutils(request):
     return render(request,'index.html',params)
 
 def textutils2(request):
-    return render(request,'index2.html')
+    return render(request,'index3.html')
 
 def analyze(request):
     djtext=request.GET.get('text','default')
@@ -33,22 +33,22 @@ def analyze(request):
             if char not in punc:
                 analyzed_text+=char
         params={'purpose':'remove punctuations','analyzed_text':analyzed_text}
-        return render(request,'analyze.html',params)
+        return render(request,'analyze2.html',params)
     elif uppercase=='on':
         analyzed_text=''
         for char in djtext:
             analyzed_text+=char.upper()
         params={'purpose':'upper case','analyzed_text':analyzed_text}
-        return render(request,'analyze.html',params)
+        return render(request,'analyze2.html',params)
     elif removenewline=='on':
         analyzed_text=''
         for char in djtext:
             if(not(char=='\n')):
                 analyzed_text+=char
         params={'purpose':'remove newLine','analyzed_text':analyzed_text}
-        return render(request,'analyze.html',params)
+        return render(request,'analyze2.html',params)
     elif countchar=='on':
         params={'purpose':'count characters','analyzed_text':len(djtext)}
-        return render(request,'analyze.html',params)
+        return render(request,'analyze2.html',params)
     else:
         return HttpResponse('Error')
