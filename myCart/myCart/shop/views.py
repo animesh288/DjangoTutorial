33457725fe88ff1efd4565ada598e65b996885f7
animesh288 <animesh.satish@gmail.com>
2022-10-09 19:25:25 +0530

    models

--- myCart/myCart/shop/views.py
+++ myCart/myCart/shop/views.py
@@ -6,16 +6,22 @@ def index(request):
     return render(request,'shop/index.html')
 
 def about(request):
+    return HttpResponse("about")
 
 def contact(request):
+    return HttpResponse("contact")
 
 def tracker(request):
+    return HttpResponse("track")
 
 def search(request):
+    return HttpResponse("search")
 
 def productView(request):
+    return HttpResponse("prod")
 
 def checkout(request):
+    return HttpResponse("checkout")
 
 
 
