057451a0e9eb80f4dd2ad12535daa93b7fb94f5e
animesh288 <animesh.satish@gmail.com>
2022-10-10 02:23:05 +0530

    added media directory

--- myCart/myCart/shop/models.py
+++ myCart/myCart/shop/models.py
@@ -7,3 +7,10 @@ class Product(models.Model):
     product_name=models.CharField(max_length=50)
     description=models.CharField(max_length=300)
     publish_date=models.DateField()
+    category=models.CharField(max_length=50,default="")
+    sub_category=models.CharField(max_length=50,default="")
+    price=models.IntegerField(default=0)
+    image=models.ImageField(upload_to="shop/images",default="")
+
+    def __str__(self):
+        return self.product_name
